ccf9d1455368654a4f92db682f5af399ac07ba01
Keith Roan <11397454+kmroan@users.noreply.github.com>
2026-02-07 20:38:51 -0500

    idk

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import os
+import os, argparse
 from dotenv import load_dotenv
 from google import genai
 
@@ -9,13 +9,15 @@ def main():
     api_key = os.environ.get("GEMINI_API_KEY")
     if api_key is None:
         raise RuntimeError("API key missing")
+    parser = argparse.ArgumentParser(description="AI Slop")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    args = parser.parse_args()
     client = genai.Client(api_key=api_key)
-    prompt = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
-    response = client.models.generate_content(model="gemini-2.5-flash", contents=prompt)
+    response = client.models.generate_content(model="gemini-2.5-flash", contents=args.user_prompt)
     if response.usage_metadata is None:
         raise RuntimeError("No response :/")
     
-    print(f"User prompt: {prompt}")
+    print(f"User prompt: {args.user_prompt}")
     print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
     print(f"Response tokens: {(response.usage_metadata.total_token_count - response.usage_metadata.prompt_token_count)} ")
     print(f"Response:\n{response.text}")
